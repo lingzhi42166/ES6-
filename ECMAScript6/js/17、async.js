00baa769e758cc0ddb3398e1ff40f01507ee1e68
Your Name <you@example.com>
2020-03-12 15:20:07 +0800

    new

--- ECMAScript6/js/17、async.js
+++ ECMAScript6/js/17、async.js
@@ -6,6 +6,12 @@
     }
     async表示函数里有异步操作，await表示紧跟在后面的表达式需要等待结果。
     async函数返回一个 Promise 对象
+    return语句返回的值，会成为then方法回调函数的参数。
+    内部抛出错误，会导致返回的 Promise 对象变为reject状态
+
+    正常情况下，await命令后面是一个 Promise 对象
+
+    
 */
 
 //async语法
@@ -19,3 +25,10 @@ const asyncTest = async function () {
 };
 console.log(asyncTest);
 console.log(asyncTest());//返回的是resolve状态的promise对象
+async function timeout(ms) {
+    // await new Promise((resolve) => {
+    //   setTimeout(resolve, ms);
+    // });
+    
+}
+console.log(timeout())
